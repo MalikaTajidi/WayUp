2900cf499477a7bbe6fc31800b29ba206de38f72
MalikaTajidi <tajidimalika@gmail.com>
2025-05-10 12:54:31 +0200

    company-suggestion client side

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { CompanySuggestionComponent } from './company-suggestion/company-suggestion.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
+    { path: 'company-suggestion', component: CompanySuggestionComponent},
 ];
---
+++ frontend/src/app/company-suggestion/company-suggestion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-suggestion',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-suggestion.component.html',
+  styleUrl: './company-suggestion.component.css'
+})
+export class CompanySuggestionComponent {
+
+}
