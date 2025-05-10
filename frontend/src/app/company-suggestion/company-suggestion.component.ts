b975483c7a2e4dfe14952e0108d425e893d10f65
MalikaTajidi <tajidimalika@gmail.com>
2025-05-10 13:02:50 +0200

    styling the Companies Card

--- frontend/src/app/company-suggestion/company-suggestion.component.ts
+++ frontend/src/app/company-suggestion/company-suggestion.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { CompanySuggestionService } from '../services/company-suggestion/company-suggestion.service';
+import { CompanySuggestion } from '../models/company-suggestion';
 
 @Component({
   selector: 'app-company-suggestion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './company-suggestion.component.html',
   styleUrl: './company-suggestion.component.css'
 })
-export class CompanySuggestionComponent {
+export class CompanySuggestionComponent //implements OnInit {
+ {
+   companies: CompanySuggestion[] = [];
 
-}
+  ngOnInit() {
+    // Dummy data for testing
+    this.companies = [
+      {
+        id: 1,
+        name: 'TechNova',
+        description: 'Innovative solutions in cloud computing.',
+        industry: 'Information Technology',
+        location: 'San Francisco, CA',
+        size: '201-500',
+        foundedYear: 2015
+      },
+      {
+        id: 2,
+        name: 'GreenFields',
+        description: 'Sustainable agriculture and food production.',
+        industry: 'Agriculture',
+        location: 'Austin, TX',
+        size: '51-200',
+        foundedYear: 2010
+      }
+    ];
+  }
+
+ }
+
+
--- frontend/src/app/services/company-suggestion/company-suggestion.service.ts
+++ frontend/src/app/services/company-suggestion/company-suggestion.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CompanySuggestion } from '../../models/company-suggestion';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanySuggestionService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/company-suggestions';
+
+  constructor(private http: HttpClient) { }
+
+    getSuggestions(userId: number): Observable<CompanySuggestion[]> {
+    return this.http.get<CompanySuggestion[]>(this.apiUrl);
+  }
 }
